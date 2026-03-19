2992f20d996acf567943e845a7b5f412f2d9ba95
Ignacio Mariano <ignaciomarianomosca@gmail.com>
2026-03-18 21:19:50 -0300

    now is possible  to ask for the sale to whatssap

--- src/app/promociones/promociones.component.ts
+++ src/app/promociones/promociones.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PromocionesService, Promocion } from '../services/promocionesService';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-promociones',
@@ -19,5 +20,12 @@ export class PromocionesComponent implements OnInit {
       next: (data) => this.promociones = data,
       error: () => this.promociones = [] // si falla el fetch, no muestra nada
     });
+  }
+   getWhatsappLink(promo: Promocion): string {
+    const phone = environment.whatsappNumber;
+    const message = encodeURIComponent(
+      `¡Hola! 👋 Vi la promoción *${promo.titulo}* en su página y me gustaría obtener más información.`
+    );
+    return `https://wa.me/${phone}?text=${message}`;
   }
 }
